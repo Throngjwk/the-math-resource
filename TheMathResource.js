36960904717e1053f393caa8ead74e38b1802daf
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-17 10:06:24 -0300

    cool

--- TheMathResource.js
+++ TheMathResource.js
@@ -16,16 +16,37 @@ var init = () => {
     currency = theory.createCurrency("n", "n");
     currency2 = theory.createCurrency("t", "t");
 
+    // a1
+    {
+        let getDesc = (level) => "A_1=" + getA1(level).toString(0);
+        a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(10, Math.log2(2))));
+        a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
+        a1.getInfo = (amount) => Utils.getMathTo(getDesc(a1.level), getDesc(a1.level + amount));
+    }
+
+    updateAvailability();
+}
+
+var updateAvailability = () => {
+    //COOL
+}
+
+var tick = (elapsedTime, multiplier) => {
+    let dt = BigNumber.from(elapsedTime * multiplier);
+    let bonus = theory.publicationMultiplier;
+    currency.value += dt * currency2.value * bonus * getA1(a1.level)
 }
 
 var getTau = () => currency.value;
 
 var getPrimaryEquation = () => {
-    let result = "A_1\\sqrt{A_3}";
+    let result = "\\dot{n} = A_1\\sqrt{A_3}";
 
     result += " \\times t"
 
     return result;
 }
+
+var getA1 = (level) => BigNumber.from(level)
 
 init();
