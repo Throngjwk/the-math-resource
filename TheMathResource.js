213018bea54ddf6d50a1a2a5e7416b32b821e724
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-19 15:30:42 -0300

    COOL BUG

--- TheMathResource.js
+++ TheMathResource.js
@@ -51,6 +51,14 @@ var init = () => {
         b1.getInfo = (amount) => Utils.getMathTo(getDesc(b1.level), getDesc(b1.level + amount));
     }
 
+    // b2
+    {
+        let getDesc = (level) => "B_2=" + getB1(level).toString(0);
+        b2 = theory.createUpgrade(4, currency, new FirstFreeCost(new ExponentialCost(1e24, Math.log2(123456789))));
+        b2.getDescription = (_) => Utils.getMath(getDesc(b2.level));
+        b2.getInfo = (amount) => Utils.getMathTo(getDesc(b2.level), getDesc(b2.level + amount));
+    }
+
     /////////////////
     //// Achievements
     achievement1 = theory.createAchievement(0, "Im After Played!", "Reach 1 A1 Level.", () => a1.level > 0);
@@ -60,10 +68,12 @@ var init = () => {
     achievement5 = theory.createAchievement(4, "A2 Mulit?", "Reach 1 A2 Level.", () => a2.level > 0);
     achievement6 = theory.createAchievement(5, "Im Millions", "Make n(t) => 1,000,000", () => currency.value > 1e6);
     achievement7 = theory.createAchievement(6, "Defalut This!", "Reach 10 A2 Level.", () => a2.level > 9);
-    achievement8 = theory.createAchievement(7, "Im Billions", "Make n(t) => 1e9 Reward: Unlock new Upgrades.", () => currency.value > 1e9);
+    achievement8 = theory.createAchievement(7, "Im Billions (Unlock I)", "Make n(t) => 1e9 Reward: Unlock new Upgrades.", () => currency.value > 1e9);
     achievement9 = theory.createAchievement(8, "A3 Mulit?", "Reach 1 A3 Level.", () => a3.level > 0);
     achievement10 = theory.createAchievement(9, "Last of Kind long", "Reach 4 A3 Level. Reward: Unlock New Letter.", () => a3.level > 3);
     achievement11 = theory.createAchievement(10, "B1 Power await", "Reach 1 B1 Level.", () => b1.level > 0);
+    achievement12 = theory.createAchievement(11, "Dialogue Squared", "Make n(t) => 1e20", () => currency.value > 1e9);
+    achievement13 = theory.createAchievement(12, "5 B1 (QoL I & Unlock II)", "Reach 5 B1 Level. Reward:t Exponent by 0.5 and Unlock new B2.", () => b1.level > 4);
 
     updateAvailability();
 }
@@ -71,6 +81,7 @@ var init = () => {
 var updateAvailability = () => {
     a3.isAvailable = currency.value > 1e9;
     b1.isAvailable = a3.level > 3
+    b2.isAvailable = b1.level > 4
 }
 
 var tick = (elapsedTime, multiplier) => {
@@ -83,7 +94,11 @@ var tick = (elapsedTime, multiplier) => {
 var getPrimaryEquation = () => {
     let result = "\\dot{n} = A_1";
 
+    if (b1.level > 3) result += "^{B_2}"
+
     result += " \\times t"
+
+    if (b1.level > 3) result += "^{1.5}"
 
     if (a3.level > 3) result += " \\times 256^{B_1}"
 
@@ -99,5 +114,6 @@ var getA1 = (level) => BigNumber.from(level) * getA2(a2.level)
 var getA2 = (level) => BigNumber.from(level + 1) * getA3(a3.level)
 var getA3 = (level) => BigNumber.from(level + 1)
 var getB1 = (level) => BigNumber.from(level)
+var getB2 = (level) => BigNumber.from(level + 1)
 
 init();
